264157d4dc6c35b2476c4d783f8aae90c86ba5db
Christine Contreras <christineec23@yahoo.com>
2022-02-25 16:30:05 -0600

    change how urls for homepage categories are pulled

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -9,14 +9,28 @@ import Hero from '../components/home/Hero'
 import Carousel from './Carousel'
 import FeaturedOneColumn from '../components/home/FeaturedOneColumn'
 import FeaturedTwoColumn from '../components/home/FeaturedTwoColumn'
-
+import { useRecoilValue } from 'recoil'
+import { categoriesAtom } from '../atoms/atoms'
 const Home = () => {
+  const categories = useRecoilValue(categoriesAtom)
+  const [cat1, setCat1] = React.useState(null)
+  const [cat2, setCat2] = React.useState(null)
+  const [cat3, setCat3] = React.useState(null)
+
+  React.useEffect(() => {
+    if (categories.length !== 0) {
+      setCat1(categories.find((cat) => cat.name === 'View All'))
+      setCat2(categories.find((cat) => cat.name === 'Rings'))
+      setCat3(categories.find((cat) => cat.name === 'Necklaces'))
+    }
+  }, [categories])
+
   const categoryOneColumn = {
     image: image1,
     background: image2,
     title: 'Every piece is crafted by hand',
     text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.',
-    url: '/category/8',
+    url: `/category/${cat1?.id}`,
   }
 
   const categoriesTwoColumns = [
@@ -24,13 +38,13 @@ const Home = () => {
       image: image3,
       title: 'One Of-A-Kind Rings',
       category: 'rings',
-      url: '/category/3',
+      url: `/category/${cat2?.id}`,
     },
     {
       image: image4,
       title: 'Ethereal Necklaces',
       category: 'necklaces',
-      url: '/category/4',
+      url: `/category/${cat3?.id}`,
     },
   ]
 
